108231f378e9cbbbf879a3162d15484fdcfdc393
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2017-10-11 18:41:35 +0000

    add version information to dump

--- dump.go
+++ dump.go
@@ -114,6 +114,7 @@ func (g *game) SortedProjectiles() consumableSlice {
 
 func (g *game) Dump() string {
 	buf := &bytes.Buffer{}
+	fmt.Fprintf(buf, " -- Boohu version %s character file --\n\n", Version)
 	if g.Wizard {
 		fmt.Fprintf(buf, "**WIZARD MODE**\n")
 	}
@@ -269,6 +270,7 @@ func (g *game) DumpedKilledMonsters() string {
 
 func (g *game) SimplifedDump() string {
 	buf := &bytes.Buffer{}
+	fmt.Fprintf(buf, " ♣ Boohu version %s play summary ♣\n\n", Version)
 	if g.Wizard {
 		fmt.Fprintf(buf, "**WIZARD MODE**\n")
 	}
