9bd8325905075be82a42f469eb7015ecf576c246
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2017-10-23 20:25:55 +0000

    pluralize noise message for >= 1 not > 1 noises

--- los.go
+++ los.go
@@ -204,7 +204,7 @@ func (g *game) ComputeNoise() {
 		}
 	}
 	if count > 0 {
-		if count > 1 {
+		if count >= 1 {
 			g.Printf("You hear some footsteps (x%d).", count)
 		} else {
 			g.Print("You hear some footsteps.")
