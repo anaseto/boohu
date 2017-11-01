6670fab7fda57d5248be25a8df485444fcd5b0eb
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2017-11-01 13:35:08 +0000

    enable auto-explore interruption in Windows (works at least on one windows machine)

--- main.go
+++ main.go
@@ -1120,21 +1120,22 @@ func (ui *termui) SelectRod(g *game, ev event) error {
 func (ui *termui) ExploreStep(g *game) bool {
 	next := make(chan bool)
 	var stop bool
-	if runtime.GOOS != "windows" {
-		// strange bugs it seems, cannot test myself, so disable on windows
-		go func() {
-			time.Sleep(10 * time.Millisecond)
-			ui.Interrupt()
-		}()
-		go func() {
-			err := ui.PressAnyKey()
-			interrupted := err != nil
-			next <- !interrupted
-		}()
-		stop = <-next
-	} else {
+	//if runtime.GOOS != "windows" {
+	// strange bugs it seems, cannot test myself, so disable on windows
+	// -> Enable this again, works at least on one particular windows cmd
+	go func() {
 		time.Sleep(10 * time.Millisecond)
-	}
+		ui.Interrupt()
+	}()
+	go func() {
+		err := ui.PressAnyKey()
+		interrupted := err != nil
+		next <- !interrupted
+	}()
+	stop = <-next
+	//} else {
+	//time.Sleep(10 * time.Millisecond)
+	//}
 	ui.DrawDungeonView(g, false)
 	return stop
 }
