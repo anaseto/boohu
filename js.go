9d32675e842dc8c1f489ade7e931eeea99457d2b
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2017-11-14 14:14:35 +0000

    most stuff in js version now kinda works

--- js.go
+++ js.go
@@ -72,20 +72,56 @@ type UICell struct {
 }
 
 func (c uicolor) String() string {
-	// TODO
-	return "color"
+	color := "#002b36"
+	switch c {
+	case 0:
+		color = "#073642"
+	case 1:
+		color = "#dc322f"
+	case 2:
+		color = "#859900"
+	case 3:
+		color = "#b58900"
+	case 4:
+		color = "#268bd2"
+	case 5:
+		color = "#d33682"
+	case 6:
+		color = "#2aa198"
+	case 7:
+		color = "#eee8d5"
+	case 8:
+		color = "#002b36"
+	case 9:
+		color = "#cb4b16"
+	case 10:
+		color = "#586e75"
+	case 11:
+		color = "#657b83"
+	case 12:
+		color = "#839496"
+	case 13:
+		color = "#6c71c4"
+	case 14:
+		color = "#93a1a1"
+	case 15:
+		color = "#fdf6e3"
+	}
+	return color
 }
 
 type termui struct {
-	cells  []UICell
-	cursor position
+	cells      []UICell
+	backBuffer []UICell
+	cursor     position
+	display    *js.Object
 }
 
 const (
 	UIWidth = 103
 	//UIWidth = 10
-	UIHeigth = 27
-	//UIHeigth = 5
+	UIHeight = 27
+	//UIHeight = 5
 )
 
 func (ui *termui) GetIndex(x, y int) int {
@@ -112,7 +148,7 @@ func init() {
 }
 
 func (ui *termui) Init() error {
-	ui.cells = make([]UICell, UIWidth*UIHeigth)
+	ui.cells = make([]UICell, UIWidth*UIHeight)
 	js.Global.Get("document").Call("addEventListener", "keypress", func(e *js.Object) {
 		select {
 		case <-wants:
@@ -122,7 +158,12 @@ func (ui *termui) Init() error {
 		}
 	})
 	ui.ResetCells()
-	js.Global.Get("document").Call("getElementById", "game").Set("innerHTML", "game screen")
+	ui.backBuffer = make([]UICell, UIWidth*UIHeight)
+	//ui.display = js.Global.Get("ROT").Call("Display", map[string]int{"width": UIWidth, "height": UIHeight})
+	//ui.display = js.Global.Get("ROT").Call("Display")
+	//container := ui.display.Call("getContainer")
+	//js.Global.Get("document").Get("body").Call("appendChild", container)
+	//ui.display.Call("draw", 5, 4, "@")
 	// TODO: init pre?
 	return nil
 }
@@ -132,6 +173,7 @@ func (ui *termui) Close() {
 }
 
 func (ui *termui) PostInit() {
+	SolarizedPalette()
 	ui.HideCursor()
 }
 
@@ -141,7 +183,18 @@ func (ui *termui) Clear() {
 
 func (ui *termui) Flush() {
 	buf := &bytes.Buffer{}
+	canvas := js.Global.Get("document").Call("getElementById", "gamecanvas")
+	ctx := canvas.Call("getContext", "2d")
+	ctx.Set("font", "12px monospace")
+	mesure := ctx.Call("measureText", "W")
+	width := mesure.Get("width").Int() + 1
+	//height := mesure.Get("height")
+	//ctx.Set("fillStyle", "#002b36")
+	//ctx.Call("fillRect", 0, 0, UIWidth*10, UIHeight*10)
 	for i := 0; i < len(ui.cells); i++ {
+		if ui.cells[i] == ui.backBuffer[i] {
+			continue
+		}
 		cell := ui.cells[i]
 		// TODO: print newlines and html markup
 		if i%UIWidth == 0 {
@@ -150,8 +203,13 @@ func (ui *termui) Flush() {
 		if cell.r == ' ' {
 			cell.r = ' '
 		}
-		//fmt.Fprintf(buf, "<span class=\"%s %s\">%c</span>", cell.fg, cell.bg, cell.r)
-		fmt.Fprintf(buf, "%c", cell.r)
+		x, y := ui.GetPos(i)
+		ctx.Set("fillStyle", cell.bg.String())
+		ctx.Call("fillRect", width*x, 17*y, width, 17)
+		ctx.Set("fillStyle", cell.fg.String())
+		ctx.Call("fillText", string(cell.r), width*x, 17*y+12)
+		//ui.display.Call("draw", x, y, string(cell.r))
+		ui.backBuffer[i] = cell
 	}
 	//ui.MoveTo(ui.cursor.X, ui.cursor.Y)
 	//if ui.cursor.X >= 0 && ui.cursor.Y >= 0 {
@@ -159,7 +217,8 @@ func (ui *termui) Flush() {
 	//} else {
 	//fmt.Fprintf(buf, "\x1b[?25l")
 	//}
-	js.Global.Get("document").Call("getElementById", "game").Set("innerHTML", buf.String())
+	//ctx.Call("fillText", buf.String(), 10, 10)
+	//js.Global.Get("document").Call("getElementById", "game").Set("innerHTML", buf.String())
 }
 
 func (ui *termui) HideCursor() {
@@ -226,14 +285,17 @@ func (ui *termui) PlayerTurnEvent(g *game, ev event) (err error, again, quit boo
 	again = true
 	r := ui.ReadChar()
 	switch r {
+	case 'S', 'Q', '#':
+		err = errors.New("Command not available (still) for the js version.")
+		return nil, true, false
 	case 'W':
 		ui.EnterWizard(g)
 		return nil, true, false
-	case 'Q':
-		if ui.Quit(g) {
-			return nil, false, true
-		}
-		return nil, true, false
+		//case 'Q':
+		//if ui.Quit(g) {
+		//return nil, false, true
+		//}
+		//return nil, true, false
 	}
 	err, again, quit = ui.HandleCharacter(g, ev, r)
 	if err != nil {
