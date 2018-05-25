c0b0f1cab8df66f5584d2265496b100a93d4cc68
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2018-05-25 19:26:17 +0000

    little adjustment to monster natural awakening rate

--- monster.go
+++ monster.go
@@ -641,7 +641,7 @@ func (m *monster) HandleTurn(g *game, ev event) {
 	mpos := m.Pos
 	m.MakeAware(g)
 	if m.State == Resting {
-		wander := RandInt(250 + 6*Max(800-(g.DepthPlayerTurn+1), 0))
+		wander := RandInt(100 + 6*Max(800-(g.DepthPlayerTurn+1), 0))
 		if wander == 0 {
 			m.NaturalAwake(g)
 		}
