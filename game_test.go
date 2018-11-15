08b2b728e255e3430b28dc7ef5ef73067b74128c
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2018-11-15 08:36:45 +0000

    fix test

--- game_test.go
+++ game_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestInitLevel(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		g := &game{}
-		for depth := 0; depth < 13; depth++ {
+		for depth := 0; depth < 11; depth++ {
 			g.Depth = depth
 			g.InitLevel()
 		}
