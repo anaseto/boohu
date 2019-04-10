72994c3606e7b76cc96a48e7c6b7d092b3b05943
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2019-04-10 09:33:39 +0000

    tweak to simplified dump

--- dump.go
+++ dump.go
@@ -265,7 +265,6 @@ func (g *game) SimplifedDump(err error) string {
 	} else {
 		fmt.Fprintf(buf, "You are exploring depth %d of Hareka's Underground.\n", g.Depth)
 	}
-	fmt.Fprintf(buf, "You had %d bananas.\n", g.Player.Bananas)
 	fmt.Fprintf(buf, "You spent %.1f turns in the Underground.\n", float64(g.Turn)/10)
 	maxDepth := Max(g.Depth, g.ExploredLevels)
 	s := "s"
