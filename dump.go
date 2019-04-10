ee793f86687846a40e93138b926d12d1f55bbfcc
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2019-04-10 09:31:28 +0000

    the number of monsters who died isn't a very interesting statistics now

--- dump.go
+++ dump.go
@@ -91,7 +91,7 @@ func (g *game) Dump() string {
 	}
 	fmt.Fprintf(buf, "\n\n")
 	fmt.Fprintf(buf, "Miscellaneous:\n")
-	fmt.Fprintf(buf, "%d monsters died.\n", g.Stats.Killed)
+	//fmt.Fprintf(buf, "%d monsters died.\n", g.Stats.Killed)
 	fmt.Fprintf(buf, "You spent %.1f turns in the Underground.\n", float64(g.Turn)/10)
 	maxDepth := Max(g.Depth, g.ExploredLevels)
 	s := "s"
