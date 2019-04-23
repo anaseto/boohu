d953e7286ef887bae7baf7532510f6969fd7f9eb
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2019-04-23 15:02:47 +0000

    comment unused targeting code

--- target.go
+++ target.go
@@ -57,169 +57,169 @@ func (ex *examiner) Done() bool {
 	return ex.done
 }
 
-type chooser struct {
-	done         bool
-	area         bool
-	minDist      bool
-	needsFreeWay bool
-	free         bool
-	flammable    bool
-	wall         bool
-}
+//type chooser struct {
+//done         bool
+//area         bool
+//minDist      bool
+//needsFreeWay bool
+//free         bool
+//flammable    bool
+//wall         bool
+//}
 
-func (ch *chooser) ComputeHighlight(g *game, pos position) {
-	g.ComputeRayHighlight(pos)
-	if !ch.area {
-		return
-	}
-	neighbors := g.Dungeon.FreeNeighbors(pos)
-	for _, pos := range neighbors {
-		g.Highlight[pos] = true
-	}
-}
+//func (ch *chooser) ComputeHighlight(g *game, pos position) {
+//g.ComputeRayHighlight(pos)
+//if !ch.area {
+//return
+//}
+//neighbors := g.Dungeon.FreeNeighbors(pos)
+//for _, pos := range neighbors {
+//g.Highlight[pos] = true
+//}
+//}
 
-func (ch *chooser) Reachable(g *game, pos position) bool {
-	return g.Player.LOS[pos]
-}
+//func (ch *chooser) Reachable(g *game, pos position) bool {
+//return g.Player.LOS[pos]
+//}
 
-func (ch *chooser) Action(g *game, pos position) error {
-	if !ch.Reachable(g, pos) {
-		return errors.New("You cannot target that place.")
-	}
-	if ch.minDist && pos.Distance(g.Player.Pos) <= 1 {
-		return errors.New("Invalid target: too close.")
-	}
-	c := g.Dungeon.Cell(pos)
-	if c.T == WallCell {
-		return errors.New("You cannot target a wall.")
-	}
-	if (ch.area || ch.needsFreeWay) && !ch.freeWay(g, pos) {
-		return errors.New("Invalid target: there are monsters in the way.")
-	}
-	mons := g.MonsterAt(pos)
-	if ch.free {
-		if mons.Exists() {
-			return errors.New("Invalid target: there is a monster there.")
-		}
-		if g.Player.Pos == pos {
-			return errors.New("Invalid target: you are here.")
-		}
-		g.Player.Target = pos
-		ch.done = true
-		return nil
-	}
-	if mons.Exists() || ch.flammable && ch.flammableInWay(g, pos) {
-		g.Player.Target = pos
-		ch.done = true
-		return nil
-	}
-	if ch.flammable && ch.flammableInWay(g, pos) {
-		g.Player.Target = pos
-		ch.done = true
-		return nil
-	}
-	if !ch.area {
-		return errors.New("You must target a monster.")
-	}
-	neighbors := pos.ValidNeighbors()
-	for _, npos := range neighbors {
-		nc := g.Dungeon.Cell(npos)
-		if !ch.wall && nc.T == WallCell {
-			continue
-		}
-		mons := g.MonsterAt(npos)
-		var okFlam bool
-		switch c.T {
-		case FoliageCell, DoorCell:
-			okFlam = true
-		}
-		if ch.flammable && okFlam || mons.Exists() || nc.T == WallCell {
-			g.Player.Target = pos
-			ch.done = true
-			return nil
-		}
-	}
-	if ch.flammable && ch.wall {
-		return errors.New("Invalid target: no monsters, walls nor flammable terrain in the area.")
-	}
-	if ch.flammable {
-		return errors.New("Invalid target: no monsters nor flammable terrain in the area.")
-	}
-	if ch.wall {
-		return errors.New("Invalid target: no monsters nor walls in the area.")
-	}
-	return errors.New("Invalid target: no monsters in the area.")
-}
+//func (ch *chooser) Action(g *game, pos position) error {
+//if !ch.Reachable(g, pos) {
+//return errors.New("You cannot target that place.")
+//}
+//if ch.minDist && pos.Distance(g.Player.Pos) <= 1 {
+//return errors.New("Invalid target: too close.")
+//}
+//c := g.Dungeon.Cell(pos)
+//if c.T == WallCell {
+//return errors.New("You cannot target a wall.")
+//}
+//if (ch.area || ch.needsFreeWay) && !ch.freeWay(g, pos) {
+//return errors.New("Invalid target: there are monsters in the way.")
+//}
+//mons := g.MonsterAt(pos)
+//if ch.free {
+//if mons.Exists() {
+//return errors.New("Invalid target: there is a monster there.")
+//}
+//if g.Player.Pos == pos {
+//return errors.New("Invalid target: you are here.")
+//}
+//g.Player.Target = pos
+//ch.done = true
+//return nil
+//}
+//if mons.Exists() || ch.flammable && ch.flammableInWay(g, pos) {
+//g.Player.Target = pos
+//ch.done = true
+//return nil
+//}
+//if ch.flammable && ch.flammableInWay(g, pos) {
+//g.Player.Target = pos
+//ch.done = true
+//return nil
+//}
+//if !ch.area {
+//return errors.New("You must target a monster.")
+//}
+//neighbors := pos.ValidNeighbors()
+//for _, npos := range neighbors {
+//nc := g.Dungeon.Cell(npos)
+//if !ch.wall && nc.T == WallCell {
+//continue
+//}
+//mons := g.MonsterAt(npos)
+//var okFlam bool
+//switch c.T {
+//case FoliageCell, DoorCell:
+//okFlam = true
+//}
+//if ch.flammable && okFlam || mons.Exists() || nc.T == WallCell {
+//g.Player.Target = pos
+//ch.done = true
+//return nil
+//}
+//}
+//if ch.flammable && ch.wall {
+//return errors.New("Invalid target: no monsters, walls nor flammable terrain in the area.")
+//}
+//if ch.flammable {
+//return errors.New("Invalid target: no monsters nor flammable terrain in the area.")
+//}
+//if ch.wall {
+//return errors.New("Invalid target: no monsters nor walls in the area.")
+//}
+//return errors.New("Invalid target: no monsters in the area.")
+//}
 
-func (ch *chooser) Done() bool {
-	return ch.done
-}
+//func (ch *chooser) Done() bool {
+//return ch.done
+//}
 
-func (ch *chooser) freeWay(g *game, pos position) bool {
-	ray := g.Ray(pos)
-	tpos := pos
-	for _, rpos := range ray {
-		mons := g.MonsterAt(rpos)
-		if !mons.Exists() {
-			continue
-		}
-		tpos = mons.Pos
-	}
-	return tpos == pos
-}
+//func (ch *chooser) freeWay(g *game, pos position) bool {
+//ray := g.Ray(pos)
+//tpos := pos
+//for _, rpos := range ray {
+//mons := g.MonsterAt(rpos)
+//if !mons.Exists() {
+//continue
+//}
+//tpos = mons.Pos
+//}
+//return tpos == pos
+//}
 
-func (ch *chooser) flammableInWay(g *game, pos position) bool {
-	ray := g.Ray(pos)
-	for _, rpos := range ray {
-		if rpos == g.Player.Pos {
-			continue
-		}
-		switch g.Dungeon.Cell(rpos).T {
-		case FoliageCell, DoorCell:
-			return true
-		}
-	}
-	return false
-}
+//func (ch *chooser) flammableInWay(g *game, pos position) bool {
+//ray := g.Ray(pos)
+//for _, rpos := range ray {
+//if rpos == g.Player.Pos {
+//continue
+//}
+//switch g.Dungeon.Cell(rpos).T {
+//case FoliageCell, DoorCell:
+//return true
+//}
+//}
+//return false
+//}
 
-type wallChooser struct {
-	done    bool
-	minDist bool
-}
+//type wallChooser struct {
+//done    bool
+//minDist bool
+//}
 
-func (ch *wallChooser) ComputeHighlight(g *game, pos position) {
-	g.ComputeRayHighlight(pos)
-}
+//func (ch *wallChooser) ComputeHighlight(g *game, pos position) {
+//g.ComputeRayHighlight(pos)
+//}
 
-func (ch *wallChooser) Reachable(g *game, pos position) bool {
-	return g.Player.LOS[pos]
-}
+//func (ch *wallChooser) Reachable(g *game, pos position) bool {
+//return g.Player.LOS[pos]
+//}
 
-func (ch *wallChooser) Action(g *game, pos position) error {
-	if !ch.Reachable(g, pos) {
-		return errors.New("You cannot target that place.")
-	}
-	ray := g.Ray(pos)
-	if len(ray) == 0 {
-		return errors.New("You are not a wall.")
-	}
-	if g.Dungeon.Cell(ray[0]).T != WallCell {
-		return errors.New("You must target a wall.")
-	}
-	if ch.minDist && g.Player.Pos.Distance(pos) <= 1 {
-		return errors.New("You cannot target an adjacent wall.")
-	}
-	for _, pos := range ray[1:] {
-		mons := g.MonsterAt(pos)
-		if mons.Exists() {
-			return errors.New("There are monsters in the way.")
-		}
-	}
-	g.Player.Target = pos
-	ch.done = true
-	return nil
-}
+//func (ch *wallChooser) Action(g *game, pos position) error {
+//if !ch.Reachable(g, pos) {
+//return errors.New("You cannot target that place.")
+//}
+//ray := g.Ray(pos)
+//if len(ray) == 0 {
+//return errors.New("You are not a wall.")
+//}
+//if g.Dungeon.Cell(ray[0]).T != WallCell {
+//return errors.New("You must target a wall.")
+//}
+//if ch.minDist && g.Player.Pos.Distance(pos) <= 1 {
+//return errors.New("You cannot target an adjacent wall.")
+//}
+//for _, pos := range ray[1:] {
+//mons := g.MonsterAt(pos)
+//if mons.Exists() {
+//return errors.New("There are monsters in the way.")
+//}
+//}
+//g.Player.Target = pos
+//ch.done = true
+//return nil
+//}
 
-func (ch *wallChooser) Done() bool {
-	return ch.done
-}
+//func (ch *wallChooser) Done() bool {
+//return ch.done
+//}
