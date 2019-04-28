b85b33b6109c23c87e780d1fb27a85f973aac049
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2019-04-28 10:46:47 +0000

    shorten a message

--- objects.go
+++ objects.go
@@ -564,7 +564,7 @@ func (g *game) EquipItem() error {
 	}
 	if oitem != NoItem {
 		g.Objects.Items[g.Player.Pos] = oitem
-		g.Printf("You equip %s, leaving %s on the ground.", it.ShortDesc(g), oitem.ShortDesc(g))
+		g.Printf("You equip %s, leaving %s.", it.ShortDesc(g), oitem.ShortDesc(g))
 		g.StoryPrintf("You equip %s, leaving %s.", it.ShortDesc(g), oitem.ShortDesc(g))
 	} else {
 		delete(g.Objects.Items, g.Player.Pos)
