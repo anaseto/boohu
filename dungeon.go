2e607e772be575acf9441f7153ff9c66395bebdd
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2019-05-02 12:23:02 +0000

    a little less bananas

--- dungeon.go
+++ dungeon.go
@@ -1230,7 +1230,7 @@ func (dg *dgen) AddSpecial(g *game, ml maplayout) {
 		dg.GenBarrierStone(g)
 	}
 	bananas := 2
-	if ml == RandomWalkTreeCave && g.Depth < MaxDepth {
+	if ml == RandomWalkTreeCave && g.Depth < MaxDepth || g.Depth == 5 || g.Depth == 8 {
 		bananas--
 	}
 	for i := 0; i < bananas; i++ {
