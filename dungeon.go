eacbd2f82664cb46bbe987e5459b6e594caa3c98
Yon <anaseto@bardinflor.perso.aquilenet.fr>
2019-05-02 12:54:50 +0000

    minor tweak to bananas distribution

--- dungeon.go
+++ dungeon.go
@@ -1230,7 +1230,7 @@ func (dg *dgen) AddSpecial(g *game, ml maplayout) {
 		dg.GenBarrierStone(g)
 	}
 	bananas := 2
-	if ml == RandomWalkTreeCave && g.Depth < MaxDepth || g.Depth == 5 || g.Depth == 8 {
+	if ml == RandomWalkTreeCave && g.Depth < MaxDepth || g.Depth == 6 || g.Depth == 8 || g.Depth == 2 {
 		bananas--
 	}
 	for i := 0; i < bananas; i++ {
